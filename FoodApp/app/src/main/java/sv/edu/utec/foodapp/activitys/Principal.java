4224e39576f3e1e52253d7464a815ee01bec5699
CesarBlanc07 <1709222019@mail.utec.edu.sv>
2023-03-01 18:00:23 -0600

    FoodApp

---
+++ FoodApp/app/src/main/java/sv/edu/utec/foodapp/activitys/Principal.java
@@ -0,0 +1,18 @@
+package sv.edu.utec.foodapp.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import sv.edu.utec.foodapp.R;
+
+public class Principal extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
